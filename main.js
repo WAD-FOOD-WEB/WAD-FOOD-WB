60d33e53c240bb86f70384438d5dcd5714fa38d2
WAD-FOOD-WEB <sps43318j@gmail.com>
2024-08-08 13:14:50 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function toggleDropdown() {
+    document.getElementById("mydpdmenu").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function(event) {
+    if (!event.target.matches('.dpdbtn')) {
+        var dropdowns = document.getElementsByClassName("dpd-content");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+
